ccb3c60ad7cedaef15039cb9433d17d6c9702f2f
Alisa010 <115400022+Alisa010@users.noreply.github.com>
2023-04-01 00:12:13 +0400

    Add files via upload

--- hw-2022-11-19/main.cpp
+++ hw-2022-11-19/main.cpp
@@ -1,1 +1,26 @@
+/*"Задача о Бруте":
 
+Напишите программу, в которой родительский процесс порождает дочерний процесс, который должeн убить своего родителя. Проверить может ли дочерний процесс убить своего родителя, если да, то что станет с дочерним процессом.
+---
+Write a program in which a parent process create a child process that must "kill" its father. Check if the child process can kill its parent process. If so, what will happen to the child process.
+ */
+
+#include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
+
+int main(){
+  pid_t pid = fork();
+  if(pid == -1){
+    std::cout << "Error" << std::endl;
+  }
+  if(pid == 0){
+    kill(getppid(), 9);
+  }
+
+        else{
+    wait(NULL);
+  }
+
+}
